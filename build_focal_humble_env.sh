6faff00a8b171c2cf798b9019dc1df8d77a5968e
kaylor <kaylor.chen@qq.com>
2023-08-27 16:08:02 +0800

    add joy

---
+++ build_focal_humble_env.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+MAIN_DIR=$(pwd)
+ROS_DISTRO=humble
+ARCH=amd64
+DIST=$(lsb_release -c | awk '{print $2}')
+echo "Arch is $ARCH and DIST is $DIST"
+mkdir -p  $MAIN_DIR/deb
+source /opt/ros/${ROS_DISTRO}/setup.bash
+printenv | grep ROS
+#python3 get_package_path.py
+cat packages-path.txt | while read i
+do
+  if [ -d "${i}" ]; then
+    pushd $i
+    echo "--------------------------------"
+    if [ -f "COLCON_IGNORE" ]; then
+      echo "${i}/COLCON_IGNORE exist"
+    else
+      PACKAGE_NAME=$(python3 ${MAIN_DIR}/get_package_name.py)
+      rm debian -rf
+      bloom-generate rosdebian --os-name ubuntu --ros-distro ${ROS_DISTRO}
+      NOW=$(date "+%Y%m%d.%H%M%S") 
+      sed -i "0,/focal/s//focal.${NOW}/" debian/changelog 
+      DEB_PREFIX=$(grep ${NOW} debian/changelog | awk '{print $1}')
+      # DEB=$(grep ${NOW} debian/changelog | awk '{print $1}')$(grep ${NOW} debian/changelog | awk '{print $2}')${ARCH}.deb
+      # DEB=$(echo ${DEB} | sed -e 's/(/_/g' | sed -e 's/)/_/g')
+      # echo DEB=${DEB}
+      sed -i 's|dh_shlibdeps |dh_shlibdeps --dpkg-shlibdeps-params=--ignore-missing-info |' debian/rules
+      DEB_BUILD_OPTIONS='parallel=16' fakeroot debian/rules binary
+      echo -e "\033[35m move ${DEB_PREFIX} package \033[0m"
+      mv ../${DEB_PREFIX}*.deb ${MAIN_DIR}/deb
+      mv ../${DEB_PREFIX}*.ddeb ${MAIN_DIR}/deb
+      apt install -y ${MAIN_DIR}/deb/*.deb
+      fakeroot debian/rules clean
+      rm -rf debian
+    fi
+    echo "********************************"
+    popd
+  fi
+done
